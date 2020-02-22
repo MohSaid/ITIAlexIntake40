1c825aa15b18d59e557ee5003bbcd868e870696c
Mohamed Said <moh.said511@gmail.com>
2020-02-22 13:03:58 +0200

    Adding project to the Repo

---
+++ StaticTableViewDemo/StaticTableViewDemo/MyTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MyTableViewController.swift
+//  StaticTableViewDemo
+//
+//  Created by Mohamed Saeed on 2/22/20.
+//  Copyright © 2020 ITI. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+   
+}
